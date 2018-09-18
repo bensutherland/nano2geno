09858e553885245b1f93f71db7d0092bcbc9ef21
bensutherland <bensutherland7@gmail.com>
2018-09-18 09:42:50 -0700

    Update minimap2 alignment command to use samtools to sort and index

--- 01_scripts/02_align.sh
+++ 01_scripts/02_align.sh
@@ -3,13 +3,17 @@
 
 # System variables
 FASTQ_FOLDER="03_basecalled"
+MAPPED_FOLDER="04_mapped"
 
 # User-defined variables
 THREADS="7"
-GENOME="/home/ben/Documents/genomes/GCA_002872995.1_Otsh_v1.0_genomic.fna.gz"
+GENOME="/home/ben/Documents/genomes/GCF_002872995.1_Otsh_v1.0_genomic.fna"
 FASTQ="all_reads.fastq"
+BAM="all_reads.sorted.bam"
 
-# Basecall 
-minimap2 -ax map-ont $GENOME $FASTQ_FOLDER/$FASTQ > aln_new.sam 
+# Align with minimap2 (requires that ref is indexed) 
+minimap2 -ax map-ont $GENOME $FASTQ_FOLDER/$FASTQ | 
+    samtools view -bS - | samtools sort -o $MAPPED_FOLDER/$BAM - 
 
-
+# Index the output with samtools
+samtools index $MAPPED_FOLDER/$BAM
