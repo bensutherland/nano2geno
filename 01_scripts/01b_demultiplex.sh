1fc46547d2e47a578e67b1efb6592e2902771f70
bensutherland <bensutherland7@gmail.com>
2018-09-18 15:45:10 -0700

    Finalize demultiplex script, initial attempt

--- 01_scripts/01b_demultiplex.sh
+++ 01_scripts/01b_demultiplex.sh
@@ -3,21 +3,30 @@
 
 # System variables
 READS_FOLDER="03_basecalled"
+DEMULTIPLEXED_FOLDER="03b_demultiplexed"
+FORWARD_FOLDER="forward_demultiplexed"
+REVERSE_FOLDER="reverse_demultiplexed"
+
+BARCODE_FOLDER="00_archive"
+FORWARD_BARCODES="IonCode768.fa"
+REVERSE_BARCODES="IonCode768_revcomp.fa"
+
+
 
 # User-defined variables
 THREADS="7"
-
-# Demultiplex 
-
+FASTQ="all_reads.fastq"
 
 
-read_fast5_basecaller.py \
-    --input $INPUT_FOLDER \
-    --worker_threads $THREADS \
-    --save_path $OUTPUT_FOLDER \
-    --flowcell $FLOWCELL \
-    --kit $KIT \
-    -r \
-    --barcoding \
-    -o fastq
+# Demultiplex with forward barcodes 
+cutadapt -a file:$BARCODE_FOLDER/$FORWARD_BARCODES \
+    --untrimmed-output $DEMULTIPLEXED_FOLDER/$FORWARD_FOLDER/forward_untrimmed.fastq \
+    -o $DEMULTIPLEXED_FOLDER/$FORWARD_FOLDER/trimmed-{name}.fastq \
+    $READS_FOLDER/$FASTQ
+
+# Secondly, demultiplex the untrimmed with reverse complement barcodes
+cutadapt -a file:$BARCODE_FOLDER/$REVERSE_BARCODES \
+    --untrimmed-output $DEMULTIPLEXED_FOLDER/$REVERSE_FOLDER/reverse_untrimmed.fastq \
+    -o $DEMULTIPLEXED_FOLDER/$REVERSE_FOLDER/trimmed-{name}.fastq \
+    $DEMULTIPLEXED_FOLDER/$FORWARD_FOLDER/forward_untrimmed.fastq # this is the untrimmed from first 
 
