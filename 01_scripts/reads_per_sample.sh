342af63f1d305414bd08eb31648722fc26a47eb8
bensutherland <bensutherland7@gmail.com>
2018-09-19 16:19:42 -0700

    Add reporting of reads in samples and reads unassigned

--- 01_scripts/reads_per_sample.sh
+++ 01_scripts/reads_per_sample.sh
@@ -11,3 +11,10 @@ for i in $(ls $SAMPLES_FOLDER/*.fastq) ; do echo $i ; awk '{s++}END{print s/4}' 
 
 # Put into good format
 awk '{printf "%s%s", $0, (NR%2?FS:RS)}' reads_per_sample.txt > reads_per_sample2.txt
+
+# Reporting
+echo "### Total reads in samples ###"
+grep -v 'untrimmed' reads_per_sample2.txt | awk '{ SUM += $2 } END { print SUM }'
+
+echo "### Total reads unassigned ###"
+grep 'reverse_untrimmed.fastq'  reads_per_sample2.txt | awk '{ print $2 }' - 
