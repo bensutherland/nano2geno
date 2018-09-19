370326bdd0b66cef2da1443d8eb023e99e07d7c2
bensutherland <bensutherland7@gmail.com>
2018-09-19 16:18:22 -0700

    Update alignment script to automate input per fastq

--- 01_scripts/02_align.sh
+++ 01_scripts/02_align.sh
@@ -2,18 +2,33 @@
 # Align the fastq against the reference genome 
 
 # System variables
-FASTQ_FOLDER="03_basecalled"
+SAMPLE_FOLDER="04_samples"
 MAPPED_FOLDER="04_mapped"
 
 # User-defined variables
 THREADS="7"
-GENOME="/home/ben/Documents/genomes/GCF_002872995.1_Otsh_v1.0_genomic.fna"
+# GENOME="/home/ben/Documents/genomes/GCF_002872995.1_Otsh_v1.0_genomic.fna"
+GENOME="00_archive/ch_WG00004_7.20170208_extracted.fa"
 FASTQ="all_reads.fastq"
 BAM="all_reads.sorted.bam"
 
 # Align with minimap2 (requires that ref is indexed) 
-minimap2 -ax map-ont $GENOME $FASTQ_FOLDER/$FASTQ | 
-    samtools view -bS - | samtools sort -o $MAPPED_FOLDER/$BAM - 
+for file in $(ls -1 $SAMPLE_FOLDER/*.fastq )
+do
+    # 
+    echo "Aligning file $file"
+    name=$(basename $file)
 
-# Index the output with samtools
-samtools index $MAPPED_FOLDER/$BAM
+    # Align fastq
+    minimap2 -ax map-ont $GENOME $SAMPLE_FOLDER/$name | 
+        samtools view -bS - | 
+        samtools sort -o $MAPPED_FOLDER/"${name%.fastq}".bam -
+
+    # Index bam
+    samtools index $MAPPED_FOLDER/"${name%.fastq}".bam
+
+    # Collect stats on the alignment
+    samtools stats $MAPPED_FOLDER/"${name%.fastq}".bam > $MAPPED_FOLDER/"${name%.fastq}".ali.stats.txt
+    grep "^COV" $MAPPED_FOLDER/"${name%.fastq}".ali.stats.txt > $MAPPED_FOLDER/"${name%.fastq}".cov.stats.txt
+
+done
