8fb019054db46187b0d20db4feffea363a72bb3c
bensutherland <bensutherland7@gmail.com>
2019-01-25 14:14:42 -0800

    Generalize the counts per chr script

--- 01_scripts/count_aligns_per_chr.sh
+++ 01_scripts/count_aligns_per_chr.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# Align the fastq against the reference genome 
+# Count number alignments per chromosome
 
 # System variables
 MAPPED_FOLDER="04_mapped"
 
-# Align with minimap2 (requires that ref is indexed) 
+# User-defined variables
+CHR_ID="linkage_group_LG"
+
+# Count alignments per chromosome with samtools
 for file in $(ls -1 $MAPPED_FOLDER/*.bam )
 do
-    # 
+    # Reporting per sample 
     echo "Stats for $file"
     name=$(basename $file)
 
     # Count alignments per chromosome (chromosome-only, not unanchored)
-    samtools view $file | awk '{ print $3 }' - | sort -n | uniq -c | grep -vE 'NW' -  > 05_results/${name%.bam}"_align_per_chr.txt"
+    samtools view $file | awk '{ print $3 }' - | sort -n | uniq -c | grep "$CHR_ID" -  > 05_results/${name%.bam}"_align_per_chr.txt"
 
 done
 
