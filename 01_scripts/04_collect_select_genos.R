de25b7b37413d8699919dcc49608556b9e0f609b
bensutherland <bensutherland7@gmail.com>
2019-01-26 12:54:53 -0800

    Update Rscript to put data into Rubias format

--- 01_scripts/04_collect_select_genos.R
+++ 01_scripts/04_collect_select_genos.R
@@ -5,7 +5,7 @@
 # rm(list=ls())
 
 # Set working directory
-setwd("~/Documents/01_nanopore/nano2geno")
+setwd("~/Documents/01_nanopore/nano2geno_stark")
 
 #### 01. Input Data ####
 # guide file
@@ -13,6 +13,7 @@ selecting.dat <- read.delim2(file = "00_archive/Ots_amplicon_names_rubias_SNP_lo
                              , header = T)
 
 colnames(selecting.dat)
+head(selecting.dat)
 dim(selecting.dat) #296 records
 
 # empirical data file
@@ -35,15 +36,16 @@ head(data)
 # set nulls
 genos <- NULL; arr.genos <- list() ; chr <- NULL
 
-for(i in 2:nrow(data)){
+for(i in 1:nrow(data)){
   print(i)
   
-  # find the two max alleles
+  # select the two max alleles
   genos <- data[i, c(6:9)]
   print(genos)
   genos <- t(genos)
   print(genos)
   
+  # save the chromosome name of interest
   chr <- data$chrom_pos[i]
   chr <- as.character(chr)
   print(chr)
@@ -57,6 +59,7 @@ arr.genos
 # Collect data out of list
 top.genos.df <- NULL; chr <- NULL; temp <- NULL; top.genos <- NULL
 top.counts <- NULL; top.counts.df <- NULL
+
 for(i in 1:length(arr.genos)){
   
   # define chr
@@ -79,49 +82,56 @@ for(i in 1:length(arr.genos)){
   top.counts <- arr.genos[[chr]][3:4]
   print(top.counts)
   top.counts.df <- rbind(top.counts.df, top.counts)
+  colnames(top.counts.df) <- c("1","2")
   rownames(top.counts.df)[i] <- chr
   
   
 }
 
 
-top.genos.df
-top.counts.df
+#top.genos.df
+head(top.genos.df)
+#top.counts.df
+head(top.counts.df)
+
 
 #### Combine data (needs merge #todo)
 all.data <- cbind(top.genos.df, top.counts.df)
-all.data
+head(all.data)
+# all.data
 
-colnames(all.data) <- c("alt", "ref", "r.alt", "r.ref")
-all.data
+colnames(all.data) <- c("min", "maj", "r.min", "r.maj")
+head(all.data)
 
 all.data.df <- as.data.frame(as.matrix(all.data))
-all.data.df
+head(all.data.df)
 
 str(all.data.df)
 
-# Coerce to numeric
-all.data.df$r.alt <- as.numeric(as.character(all.data.df$r.alt))
-all.data.df$r.ref <- as.numeric(as.character(all.data.df$r.ref))
+# Coerce counts to numeric
+all.data.df$r.min <- as.numeric(as.character(all.data.df$r.min))
+all.data.df$r.maj <- as.numeric(as.character(all.data.df$r.maj))
 
-all.data.df$alt <- as.character(all.data.df$alt)
-all.data.df$ref <- as.character(all.data.df$ref)
+# Coerce factor to character
+all.data.df$min <- as.character(all.data.df$min)
+all.data.df$maj <- as.character(all.data.df$maj)
 
 str(all.data.df)
 
-all.data.df
+head(all.data.df)
 
 
 ## Order as largest then second largest
 all.data.df <- all.data.df[ , c(2, 4, 1, 3) ]
-all.data.df
+head(all.data.df)
 
 
 #### 03. Additional Stats ####
-#### Calculate alleleic ratio (alt / ref)
+#### Calculate alleleic ratio (min / maj)
 all.data.df$al.ratio <- round(x = 
-                                all.data.df[, "r.alt"] / (all.data.df[, "r.ref"] + all.data.df[, "r.alt"])
+                                all.data.df[, "r.min"] / (all.data.df[, "r.maj"] + all.data.df[, "r.min"])
                               , digits = 2)
+head(all.data.df)
 
 ## Backup
 # all.data.df.bck <- all.data.df
@@ -133,9 +143,9 @@ snp.call <- NULL;
 for(i in 1:nrow(all.data.df)){
   # 
   if(all.data.df$al.ratio[i] > 0.75){
-    snp.call <- "homo.alt"
+    snp.call <- "homo.min"
   } else if(all.data.df$al.ratio[i] < 0.25){
-    snp.call <- "homo.ref"
+    snp.call <- "homo.maj"
   } else {
     snp.call <- "het"
   }
@@ -145,14 +155,15 @@ for(i in 1:nrow(all.data.df)){
   all.data.df$snp.call[i] <- snp.call
   
 }
-all.data.df
+
+head(all.data.df, n = 10)
 
 #### 05. Filter based on low read count ####
 # Set minimum total read filter
 threshold <- 20
 
 for(i in 1:nrow(all.data.df)){
-  if(all.data.df$r.ref[i] + all.data.df$r.alt[i] > threshold){
+  if(all.data.df$r.maj[i] + all.data.df$r.min[i] > threshold){
     all.data.df$keep[i] <- "TRUE"
   } else all.data.df$keep[i] <- "FALSE"
 }
@@ -164,6 +175,149 @@ all.data.df$chr <- rownames(all.data.df)
 all.data.df <- all.data.df[, c(8, 1:7)]
 all.data.df
 
+table(all.data.df$keep)
+
+head(all.data.df)
+
+
+#### 06. Make geno column ####
+genos1 <- NULL; genos2 <- NULL
+
+for(i in 1:nrow(all.data.df)){
+  if(all.data.df$snp.call[i]=="homo.maj"){
+    #print("homo.maj")
+    genos1[i] <- all.data.df$maj[i]
+    genos2[i] <- all.data.df$maj[i]
+    
+  } else if(all.data.df$snp.call[i]=="het"){
+    #print("het")
+    genos1[i] <- all.data.df$maj[i]
+    genos2[i] <- all.data.df$min[i]
+  }
+}
+
+
+genos1
+genos2
+
+# Add these calls to df
+all.data.df$genos1 <- genos1
+all.data.df$genos2 <- genos2
+
+head(all.data.df)
+
+
+##### Connect back in the guide file
+all.data.w.guide.df <- merge(x = all.data.df, y = selecting.dat, by.x = "chr", by.y  = "chrom_pos")
+dim(all.data.w.guide.df) # 291 records
+head(all.data.w.guide.df)
+
+
+##### Give true geno score w/ hotspot file
+hotspot.call <- NULL
+
+for(i in 1:nrow(all.data.w.guide.df)){
+  if(all.data.w.guide.df$genos1[i]!=all.data.w.guide.df$genos2[i]){
+    hotspot.call[i] <- "het"
+    #todo: confirm this het is the same as the expected het, otherwise throw NA
+    
+    } else if( (all.data.w.guide.df$genos1[i]==all.data.w.guide.df$genos2[i]) # is a homozygote 
+                 && all.data.w.guide.df$genos1[i]==all.data.w.guide.df$ref[i]){ # is a homo.ref)
+      hotspot.call[i] <- "homo.ref"
+  
+    } else if( (all.data.w.guide.df$genos1[i]==all.data.w.guide.df$genos2[i]) # is a homozygote 
+              && all.data.w.guide.df$genos1[i]==all.data.w.guide.df$var[i]){ # is a homo.var)) {
+      hotspot.call[i] <- "homo.var"
+      }
+    }
+
+hotspot.call
+
+all.data.w.guide.w.hotspot.call.df <- cbind(all.data.w.guide.df, hotspot.call)
+head(all.data.w.guide.w.hotspot.call.df, n = 10)
+
+
+
+##### Convert to Rubias format
+rubias1 <- NULL; rubias2 <- NULL
+#scoring: homo.ref = 1,1 ; het = 1,2 ; homo.var = 2,2 
+
+# Loop
+for(i in 1:nrow(all.data.w.guide.w.hotspot.call.df)){
+  if(all.data.w.guide.w.hotspot.call.df$hotspot.call[i]=="homo.ref"){
+    rubias1[i] <- 1
+    rubias2[i] <- 1
+  } else if(all.data.w.guide.w.hotspot.call.df$hotspot.call[i]=="het"){
+    rubias1[i] <- 1
+    rubias2[i] <- 2
+  } else if(all.data.w.guide.w.hotspot.call.df$hotspot.call[i]=="homo.var"){
+    rubias1[i] <- 2
+    rubias2[i] <- 2
+  }
+}
+
+complete.df <- cbind(all.data.w.guide.w.hotspot.call.df, rubias1, rubias2)
+
+head(complete.df)
+
+### Hacky below ###
+rubias.only.1 <- complete.df[, c("chrom", "rubias1")]
+rubias.only.2 <- complete.df[, c("chrom", "rubias2")]
+
+rownames(rubias.only.1) <- rubias.only.1$chrom
+rownames(rubias.only.2) <- paste0(rubias.only.1$chrom, "_1")
+
+head(rubias.only.1)
+head(rubias.only.2)
+
+t.x.1 <- t(rubias.only.1)
+t.x.2 <- t(rubias.only.2)
+
+t.x.1[1:2, 1:5]
+t.x.2[1:2, 1:5]
+
+test <- cbind(t.x.1, t.x.2)
+dim(test)
+
+test[1:2,1:6]
+test[1:2,570:582]
+
+test.df <- as.data.frame(test)
+test.df[1:2,1:6]
+test.df[1:2,570:582]
+
+output.df <- test.df["rubias1" ,]
+output.df
+output.df.t <- t(output.df)
+
+output.df.t
+
+output.df.t <- output.df.t[ order(row.names(output.df.t)), ]
+head(output.df.t)
+
+str(output.df.t)
+
+names(output.df.t)
+final.out.df <- rbind(names(output.df.t), output.df.t)
+dim(final.out.df)
+final.out.df[1:2,1:5]
+
+
+### Data available in output.df.t
+write.table(x = final.out.df, file = "rubias_input.csv", sep=",", row.names = F, col.names = F)
+
+
+
+
 # Export results
 write.csv(x = all.data.df, file = "06_vcf/genotypes.csv", row.names = F)
+write.csv(x = all.data.w.guide.df, file = "temp.csv")
+
+
+
+
+
+
+
+
 
